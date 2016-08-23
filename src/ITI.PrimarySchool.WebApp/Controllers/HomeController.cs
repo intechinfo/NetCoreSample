ce268d6036ad4374d940cb5cd2c9d66226b52a47
Antoine Raquillet <antoine.raquillet@invenietis.com>
2016-08-23 18:10:55 +0200

    Added mvc project.

---
+++ src/ITI.PrimarySchool.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.PrimarySchool.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/ITI.PrimarySchool.WebApp/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ITI.PrimarySchool.WebApp
+{
+    public class Startup
+    {
+        public void ConfigureServices( IServiceCollection services )
+        {
+            services.AddMvc();
+        }
+
+        public void Configure( IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory )
+        {
+            loggerFactory.AddConsole();
+
+            if( env.IsDevelopment() )
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc( routes =>
+            {
+                routes.MapRoute(
+                  name: "default",
+                  template: "{controller}/{action}/{id?}",
+                  defaults: new { controller = "Home", action = "Index" } );
+            } );
+        }
+    }
+}
