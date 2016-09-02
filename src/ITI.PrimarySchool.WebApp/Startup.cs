823bba6fec835a4852638821f652bbf2ae69ff08
Antoine Raquillet <antoine.raquillet@invenietis.com>
2016-09-02 11:05:31 +0200

    Added user registration.

--- src/ITI.PrimarySchool.WebApp/Startup.cs
+++ src/ITI.PrimarySchool.WebApp/Startup.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using ITI.PrimarySchool.DAL;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -7,9 +9,22 @@ namespace ITI.PrimarySchool.WebApp
 {
     public class Startup
     {
+        public Startup( IHostingEnvironment env )
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath( env.ContentRootPath )
+                .AddJsonFile( "appsettings.json", optional: true )
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         public void ConfigureServices( IServiceCollection services )
         {
             services.AddMvc();
+            services.AddTransient( _ => new UserGateway( Configuration[ "ConnectionStrings:PrimarySchoolDB" ] ) );
+            services.AddTransient<PasswordHasher>();
         }
 
         public void Configure( IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory )
