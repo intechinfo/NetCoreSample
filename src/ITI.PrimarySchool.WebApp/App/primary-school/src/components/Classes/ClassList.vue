2888dcd206775450c1eee430460397d5a297b8fe
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2016-09-08 17:28:24 +0200

    List components.

--- src/ITI.PrimarySchool.WebApp/App/primary-school/src/components/Classes/ClassList.vue
+++ src/ITI.PrimarySchool.WebApp/App/primary-school/src/components/Classes/ClassList.vue
@@ -1,12 +1,55 @@
 <template>
     <div>
-        <h1>Gestion des classes</h1>
+        <div class="page-header">
+            <h1>Gestion des classes</h1>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-body text-right">
+                <router-link class="btn btn-primary" :to="`classes/create`"><i class="glyphicon glyphicon-plus"></i> Ajouter une classe</router-link>
+            </div>
+        </div>
+
+        <table class="table table-striped table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nom</th>
+                    <th>Niveau</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-if="classList.length == 0">
+                    <td colspan="4" class="text-center">Il n'y a actuellement aucune classe.</td>
+                </tr>
+
+                <tr v-for="i of classList">
+                    <td>{{ i.classId }}</td>
+                    <td>{{ i.name }}</td>
+                    <td>{{ i.level }}</td>
+                    <td>
+                        <router-link :to="`classes/edit/${i.classId}`"><i class="glyphicon glyphicon-pencil"></i></router-link>
+                        <a href="#" @click="deleteClass(i.classId)"><i class="glyphicon glyphicon-remove"></i></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
-    export default {
+    import { mapGetters, mapActions } from 'vuex'
 
+    export default {
+        computed: {
+            ...mapGetters(['classList'])
+        },
+
+        methods: {
+            ...mapActions(['deleteClass'])
+        }
     }
 </script>
 
