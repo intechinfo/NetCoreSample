a43952e2a21d87abfbb71fbece5a344706c5263a
Alexandre Da Silva <dev.dasilva.alexandre@gmail.com>
2016-09-08 17:27:04 +0200

    Add getters for lists.

--- src/ITI.PrimarySchool.WebApp/App/primary-school/src/vuex/getters.js
+++ src/ITI.PrimarySchool.WebApp/App/primary-school/src/vuex/getters.js
@@ -2,7 +2,10 @@
 export const isLoading = ({ app }) => app.isLoading // eq. export const isLoading = state => state.app.isLoading
 
 // Classes
+export const classList = ({ classes }) => classes.classList
 
 // Students
+export const studentList = ({ students }) => students.studentList
 
 // Teachers
+export const teacherList = ({ teachers }) => teachers.teacherList
