71702b74e6b0bb7d41a129b10370116f0c4ffd6b
Antoine Raquillet <antoine.raquillet@invenietis.com>
2017-07-28 10:21:10 +0200

    Restart from scratch.

--- src/ITI.PrimarySchool.WebApp/Authentication/CookieAuthentication.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ITI.PrimarySchool.WebApp.Authentication
-{
-    public static class CookieAuthentication
-    {
-        public const string AuthenticationScheme = "PrimarySchoolCookie";
-        public const string AuthenticationType = "PrimarySchoolCookie";
-    }
-}
--- src/ITI.PrimarySchool.WebApp/Models/TeacherViewModels/AssignedClassViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ITI.PrimarySchool.WebApp.Models.TeacherViewModels
-{
-    public class AssignClassViewModel
-    {
-        public int ClassId { get; set; }
-    }
-}
